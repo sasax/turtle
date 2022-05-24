aa01af0488846abbf0af4976f4364acdbb4b5f80
sacha <sacha>
2022-05-24 18:57:06 +0200

    translate to german

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-# Python program to draw a turtle
 import turtle
 import time
 
 
-# Function that draws the turtle
+# Funktion, die die Schildkröte zeichnet
 def drawBar(t, height, color):
-    # Get turtle t to draw one bar
-    # of height
 
-    # Start filling this shape
+    # Fängt an, diese Form zu füllen
     t.fillcolor(color)
     t.begin_fill()
     t.left(90)
@@ -20,17 +17,13 @@ def drawBar(t, height, color):
     t.forward(height)
     t.left(90)
 
-    # stop filling the shape
+    # Hört auf, diese Form zu füllen
     t.end_fill()
 
 
-# Driver Code
-
-
-# Set up the window and its
-# attributes
 wn = turtle.Screen()
 
+# Array welches zu sortieren ist
 xs = [3, 6, 2, 5, 0, 9, 10, 7, 1, 4, 8]
 
 border = 10
@@ -38,69 +31,71 @@ border = 10
 
 def hex_to_RGB(hex):
     ''' "#FFFFFF" -> [255,255,255] '''
-    # Pass 16 to the integer function for change of base
+    # range(Start, Stop[, Schritt])
     return [int(hex[i:i + 2], 16) for i in range(1, 6, 2)]
 
 
 def RGB_to_hex(RGB):
+    ''' [255,255,255] -> "#FFFFFF" '''
+    # format(eingabe, 'x'),  x steht für Hexadezimal
     return "#" + format(RGB[0], 'x') + format(RGB[1], 'x') + format(RGB[2], 'x')
 
 
-def linear_gradient(n=307, start_hex="#000000", finish_hex="#FFFFFF"):
-    # Starting and ending colors in RGB form
-    s = hex_to_RGB(start_hex)
-    f = hex_to_RGB(finish_hex)
-    # Initilize a list of the output colors with the starting color
+def linear_gradient(n=307):
+    # Übersetzt Anfongsfarbe und Endfarbe in RGB-Form.
+    s = hex_to_RGB("#000000")
+    f = hex_to_RGB("#FFFFFF")
+    # Initialisiert die Startfarbe weiss
     RGB_list = ["#000000"]
     # Calcuate a color at each evenly spaced value of t from 1 to n
+    # Berechnet die Farben für jeden gleichmäßig verteilten Wert von 1 bis n
     for t in range(1, n):
-        # Interpolate RGB vector for color at the current value of t
-        curr_vector = [int(s[j] + (float(t) / (n - 1)) * (f[j] - s[j])) for j in range(3)]
-        # Add it to our list of output colors
+        #  Sätzt die Farben, um so tiefer die Zahl um so heller
+        curr_vector = [int((float(t) / (n - 1)) * 255) for j in range(3)]
+        # Fügt die Zahl in das Array ein.
         RGB_list.append(RGB_to_hex(curr_vector))
     return RGB_list
 
 
 def drawGraph(xs):
+    # Die Schildkröte zeichnet alles auf einmal
     wn.tracer(0, 0)
 
+    # findet das grösste Element der Liste heraus
     maxheight = max(xs)
+
+    # zählt wieviele Zahlen zu sortieren sind
     numbers = len(xs)
 
+    # Passt die Grösse des Anzeigefenster an
     wn.setworldcoordinates(0 - border, 0 - border,
                            40 * numbers + border,
                            maxheight + border)
 
-    # Create tess and set some attributes
     tess = turtle.Turtle()
     tess.pensize(3)
 
+    # Iteriert durch jeden Balken und zeichnet ihn auch
     for i in range(len(xs)):
         drawBar(tess, xs[i],
                 linear_gradient(len(xs))[xs[i]])
     turtle.update()
 
-    time.sleep(0.5)
+    time.sleep(1.5)
 
+
+    # löscht die Oberfläche
     turtle.clear()
-
     turtle.reset()
     turtle.clearscreen()
 
 
-def swap(array, position1, position2):
-    array[position1], array[position2] = array[position2], array[position1]
-    return array
 
-
-
-
+# Tauscht das Element von der pos1 mit dem Element von pos2 aus
 def swapPositions(list, pos1, pos2):
-    # popping both the elements from list
     first_ele = list.pop(pos1)
     second_ele = list.pop(pos2 - 1)
 
-    # inserting in each others positions
     list.insert(pos1, second_ele)
     list.insert(pos2, first_ele)
 
@@ -154,7 +149,9 @@ def bubble_sort(array):
     swapPositions(array, 2, 3)
     drawGraph(array)
     swapPositions(array, 1, 2)
+    drawGraph(array)
 
 bubble_sort(xs)
 
+#
 wn.exitonclick()
